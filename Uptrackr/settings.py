fbc7888316d3faaa9e84c4910838832e6fe8c857
Emmanuel Momoh <momohemmanuel370@gmail.com>
2024-06-10 13:34:08 +0100

    updated css

--- Uptrackr/settings.py
+++ Uptrackr/settings.py
@@ -161,11 +161,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
