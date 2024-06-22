cfd0335b3fc1ca5aa3c5329f633e9add72aed259
Emmanuel Momoh <102439386+nuel000@users.noreply.github.com>
2024-06-22 02:10:08 +0100

    Update process_manager.py

--- app/process_manager.py
+++ app/process_manager.py
@@ -2,28 +2,29 @@ import subprocess
 import signal
 
 class ProcessManager:
-    process = None
+    processes = {}  # Dictionary to store user -> process mapping
 
     @classmethod
     def start_process(cls, user, rss_user):
-        if cls.process is None or cls.process.poll() is not None:
-            # No process is running or the existing process has terminated
+        if user.username not in cls.processes or cls.processes[user.username].poll() is not None:
+            # No process is running for the user or the existing process has terminated
             script_path = '/home/nuel/uptrackr_saas/main/main.py'
             python_path = '/home/nuel/uptrackr_saas/env/bin/python'
-            cls.process = subprocess.Popen([python_path, script_path, rss_user.email, rss_user.rss_url])
-            return cls.process.pid
+            process = subprocess.Popen([python_path, script_path, rss_user.email, rss_user.rss_url])
+            cls.processes[user.username] = process
+            return process.pid
         else:
-            # A process is already running
+            # A process is already running for the user
             return None
 
     @classmethod
-    def stop_process(cls):
-        if cls.process is not None and cls.process.poll() is None:
-            # A process is running, terminate it
-            cls.process.send_signal(signal.SIGTERM)
-            cls.process.wait()
-            cls.process = None
+    def stop_process(cls, user):
+        if user.username in cls.processes and cls.processes[user.username].poll() is None:
+            # A process is running for the user, terminate it
+            cls.processes[user.username].send_signal(signal.SIGTERM)
+            cls.processes[user.username].wait()
+            del cls.processes[user.username]
             return True
         else:
-            # No process is running
+            # No process is running for the user
             return False
