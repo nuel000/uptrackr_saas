b91f48a64cd5d27129f58a10748fe58333cd1615
Emmanuel Momoh <102439386+nuel000@users.noreply.github.com>
2024-06-22 03:36:46 +0100

    Update process_manager.py

--- app/process_manager.py
+++ app/process_manager.py
@@ -17,14 +17,37 @@ class ProcessManager:
             # A process is already running for the user
             return None
 
+
     @classmethod
     def stop_process(cls, user):
         if user.username in cls.processes and cls.processes[user.username].poll() is None:
             # A process is running for the user, terminate it
-            cls.processes[user.username].send_signal(signal.SIGTERM)
-            cls.processes[user.username].wait()
-            del cls.processes[user.username]
-            return True
+            try:
+                cls.processes[user.username].send_signal(signal.SIGTERM)
+                cls.processes[user.username].wait(timeout=5)  # Timeout in seconds
+                del cls.processes[user.username]
+                return True
+            except subprocess.TimeoutExpired:
+                # If the process doesn't stop within the timeout, use SIGKILL
+                cls.processes[user.username].send_signal(signal.SIGKILL)
+                cls.processes[user.username].wait()
+                del cls.processes[user.username]
+                return True
+            except Exception as e:
+                print(f"Error stopping process for user {user.username}: {e}")
+                return False
         else:
             # No process is running for the user
             return False
+
+    # @classmethod
+    # def stop_process(cls, user):
+    #     if user.username in cls.processes and cls.processes[user.username].poll() is None:
+    #         # A process is running for the user, terminate it
+    #         cls.processes[user.username].send_signal(signal.SIGTERM)
+    #         cls.processes[user.username].wait()
+    #         del cls.processes[user.username]
+    #         return True
+    #     else:
+    #         # No process is running for the user
+    #         return False
